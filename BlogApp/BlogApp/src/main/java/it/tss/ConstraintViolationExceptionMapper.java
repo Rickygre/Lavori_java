29747b4d685e99af96d454396a79584c61319ff1
Rickygre <riccardino7@gmail.com>
2022-03-24 09:02:21 +0100

    24-03

---
+++ BlogApp/BlogApp/src/main/java/it/tss/ConstraintViolationExceptionMapper.java
@@ -0,0 +1,24 @@
+
+package it.tss;
+
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author tss
+ */
+@Provider
+public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException>{
+
+    @Override
+    public Response toResponse(ConstraintViolationException ex) {
+        Response.ResponseBuilder rb = Response.status(Response.Status.BAD_REQUEST);
+        ex.getConstraintViolations()
+                .forEach(v -> rb.header("cased-by-" + v.getPropertyPath().toString(), v.getMessage()));
+        return rb.build();
+    }
+    
+}
---
+++ BlogApp/BlogApp/src/main/java/it/tss/JAXRSConfiguration.java
@@ -0,0 +1,17 @@
+
+package it.tss;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint. Delete this class, if you are not exposing
+ * JAX-RS resources in your application.
+ *
+ * @author airhacks.com
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
